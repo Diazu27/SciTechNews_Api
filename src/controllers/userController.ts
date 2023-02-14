30abd55d7fd178540f4d59cd56d78403f1037adb
Diazu27 <AndresDiazu2001@gmail.com>
2023-02-13 21:46:58 -0600

    Add User crud endpoints

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,9 @@
 import { User } from "../models/userModel";
 
-export const getUsers = (req, res)=>{
-    res.send("Hola desde usuarios");
+export const getUsers = async(req, res)=>{
+    const userData = await User.find();
+    res.status(200).json(userData)
+
 }
 
 export const postUsers = async(req, res)=>{
@@ -11,6 +13,32 @@ export const postUsers = async(req, res)=>{
         await userdata.save();
 
         res.status(200).json({msg:"Successful",userdata})
+
+    }catch(error){
+        res.status(400).json({error: error})
+    }
+}
+
+export const UpdateUser =async (req, res) => {
+    try{
+        const userdata = await User.updateOne({_id: req.params.id}, {$set:req.body})
+        res.status(200).json({msg:"Successful",userdata})
+
+    }catch(error){
+        res.status(400).json({error: error})
+    }
+}
+
+export const getUserById =async (req, res) => {
+    const userData = await User.findById(req.params.id);
+    res.status(200).json(userData)
+}
+
+
+export const DeleteUser = async (req, res) => {
+    try{
+        const userdata = await User.deleteOne({_id: req.params.id})
+        res.status(200).json({msg:"Deleteed successful",userdata})
 
     }catch(error){
         res.status(400).json({error: error})
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,7 +1,11 @@
+import { DeleteUser, getUserById } from './../controllers/userController';
 import Express  from "express";
-import { getUsers, postUsers } from "../controllers/userController";
+import { getUsers, postUsers, UpdateUser } from "../controllers/userController";
 
 export const UserRoutes = Express.Router();
 
 UserRoutes.get('/', getUsers)
+UserRoutes.get('/:id', getUserById)
 UserRoutes.post('/', postUsers)
+UserRoutes.put('/:id', UpdateUser)
+UserRoutes.delete('/:id', DeleteUser)
