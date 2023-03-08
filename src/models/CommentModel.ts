d7349e9b8c3bb72e4dd995127464c1ba9bbd0a86
Diazu27 <AndresDiazu2001@gmail.com>
2023-03-07 23:25:46 -0600

    Fix deploy bug

--- src/models/CommentModel.ts
+++ src/models/CommentModel.ts
@@ -1,4 +1,4 @@
-import { Schema, model } from 'mongoose';
+import mongoose, { Schema, model } from 'mongoose';
 
 // Document interface
 export interface IComment {
@@ -18,4 +18,4 @@ const CommentSchema = new Schema<IComment>({
     postDate: { type: Date, default: new Date() }
 });
 
-export const Comment = model<IComment>('Comment', CommentSchema, 'Comment');
+export const Comment = mongoose.models.Comment || model<IComment>('Comment', CommentSchema, 'Comment');
